36bf70c3d1af18cd9bc228dc4468b97d0a01b3e8
Ardit Daku <arditdaku1@gmail.com>
2021-05-09 12:19:57 +0200

    Added Logo and Body style

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/header";
 
 export default function Home() {
   return (
@@ -6,6 +7,8 @@ export default function Home() {
       <Head>
         <title>Hulu</title>
       </Head>
+
+      <Header />
     </div>
   );
 }
