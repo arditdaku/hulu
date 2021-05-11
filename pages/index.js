5241b5d886506a59f034a9b26b210b24da5c43e6
Ardit Daku <arditdaku1@gmail.com>
2021-05-11 19:32:51 +0200

    Finished Header Component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Header from "../components/header";
-
+import Header from "../components/Header";
+import Nav from "../components/Nav";
 export default function Home() {
   return (
     <div>
@@ -9,6 +9,8 @@ export default function Home() {
       </Head>
 
       <Header />
+
+      <Nav />
     </div>
   );
 }
